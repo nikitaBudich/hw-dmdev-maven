54736f3c5c8b9860e808dabf4da777081bff5527
Renatka <Renatka@MacBook-Pro.local>
2023-02-03 11:00:56 +0200

    Initial commit

---
+++ common/src/test/java/org/example/common/ExampleIT.java
@@ -0,0 +1,12 @@
+package org.example.common;
+
+import org.junit.jupiter.api.Test;
+
+
+public class ExampleIT {
+    @Test
+    public void test() {
+        PrintUtil printUtil = new PrintUtil();
+        printUtil.print();
+    }
+}
---
+++ service/src/test/java/org/example/servise/TestExample.java
@@ -0,0 +1,12 @@
+package org.example.servise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestExample {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
